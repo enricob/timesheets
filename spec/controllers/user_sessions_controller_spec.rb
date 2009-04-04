7969adb96911545b4c8cd59859abcc1e37075ce4
Enrico Bianco <enricob@gmail.com>
2009-04-04 19:33:39 -0400

    Sample spec added to UserSession controller specs that uses Authlogic testing functionality

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -1,10 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require "authlogic/test_case"
 
 describe UserSessionsController do
-
+  before(:each) do
+    activate_authlogic
+  end
+  
   it "should respond to new with login page" do
     get 'new'
     response.should render_template("user_sessions/new.html.erb")
   end
-
+  
+  it "should not have a current user if nobody logged in" do
+    assert_nil @current_user
+  end
 end
