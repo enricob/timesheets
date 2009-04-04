4b51697b2172953ee12da7d8e6a3a6d1473e3422
Enrico Bianco <enricob@gmail.com>
2009-04-04 16:27:21 -0400

    basic spec for UserSessions controller: checks if "new" action returns the login page

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -2,9 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe UserSessionsController do
 
-  #Delete this example and add some real ones
-  it "should use UserSessionsController" do
-    controller.should be_an_instance_of(UserSessionsController)
+  it "should respond to new with login page" do
+    get 'new'
+    response.should render_template("views/user_session/new.html.erb")
   end
 
 end
