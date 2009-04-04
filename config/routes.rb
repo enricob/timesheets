64c666e119f82455c9645351a0ac670b0c1c7204
Enrico Bianco <enricob@gmail.com>
2009-04-04 18:22:34 -0400

    commented out default routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
   map.resources :users
 
-  map.resource :user_session, :as => "session"
+  map.resource :user_session, :as => "session", :except => "edit"
   map.root :controller => "user_sessions", :action => "new"
   
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  # map.connect ':controller/:action/:id'
+  # map.connect ':controller/:action/:id.:format'
 end
