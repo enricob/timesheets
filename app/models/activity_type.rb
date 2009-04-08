1dfde7aa0f24fe882a1c6dc5736a28e397c9d575
Enrico Bianco <enricob@gmail.com>
2009-04-08 19:19:15 -0400

    basic relationship definitions for Project and ActivityType models

--- app/models/activity_type.rb
+++ app/models/activity_type.rb
@@ -1,2 +1,4 @@
 class ActivityType < ActiveRecord::Base
+  belongs_to :project
+  validates_presence_of :name
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ActiveRecord::Base
+  has_many :activity_types
   validates_presence_of :name
 end
