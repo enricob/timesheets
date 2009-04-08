c7bb906ff4f3c22806889cce3974330a5aed5339
Enrico Bianco <enricob@gmail.com>
2009-04-08 19:38:28 -0400

    fixed dataset for Projects so that the symbol names get generated as expected

--- spec/datasets/projects_dataset.rb
+++ spec/datasets/projects_dataset.rb
@@ -1,12 +1,12 @@
 class ProjectsDataset < Dataset::Base
   def load
-    create_project :name => "Project X", :description => "Doing cool things with the power of science!"
-    create_project :name => "Project E", :description => "Protecting our green Earth"
+    create_project :name => "ProjectX", :description => "Doing cool things with the power of science!"
+    create_project :name => "ProjectE", :description => "Protecting our green Earth"
   end
 
   helpers do
     def create_project(attributes={})
-      create_record :project, attributes[:name].downcase.underscore,
+      create_record :project, attributes[:name].underscore.to_sym,
         :name => attributes[:name],
         :description => attributes[:description]
     end
