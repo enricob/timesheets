4453950f7c031511af9528f1a2f4a0e880c50b4a
Enrico Bianco <enricob@gmail.com>
2009-04-08 15:27:06 -0400

    revamped Users controller specs based on Projects controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,57 +10,151 @@ describe UsersController do
     activate_authlogic
   end
   
-  it "should reject new requests for a logged in user" do
-    UserSession.create(users(:ben))
-    get 'new'
-    response.should redirect_to("/account")
-    assert flash[:notice] == "You must be logged out to access this page"
+  def mock_user(stubs={})
+    @mock_user ||= mock_model(User, stubs)
   end
   
-  it "should reject create requests for a logged in user" do
-    UserSession.create(users(:zack))
-    post 'create'
-    response.should redirect_to("/account")
-    assert flash[:notice] == "You must be logged out to access this page"
+  describe "GET new" do
+    describe "for a logged in user" do
+      before(:each) do
+        UserSession.create(users(:ben))
+        get 'new'
+      end
+      
+      it "redirects to the user's account" do
+        response.should redirect_to("/account")
+      end
+      
+      it "sets the flash" do
+        assert flash[:notice] == "You must be logged out to access this page"
+      end
+    end
+    
+    describe "without a logged in user" do
+      before(:each) do
+        get 'new'
+      end
+      
+      it "shows the sign-up form" do
+        response.should render_template("users/new.html.erb")
+      end
+    end
   end
+  
+  describe "GET show" do
+    describe "for a logged in user" do
+      before(:each) do
+        UserSession.create(users(:zack))
+        get 'show'
+      end
+      
+      it "shows the user's account info" do
+        assert assigns[:user] == users(:zack)
+        response.should render_template("users/show.html.erb")
+      end
+    end
+    
+    describe "with no logged in user" do
+      before(:each) do
+        get 'show'
+      end
+      
+      it "redirects to the login page" do
+        response.should redirect_to("/session/new")
+      end
+    end
+  end
+  
+  describe "GET edit" do
+    describe "for a logged in user" do
+      before(:each) do
+        UserSession.create(users(:ben))
+        get 'edit'
+      end
+      
+      it "shows the account edit form for that user" do
+        assert assigns[:user] == users(:ben)
+        response.should render_template("users/edit.html.erb")
+      end
+    end
+    
+    describe "without a logged in user" do
+      before(:each) do
+        get 'edit'
+      end
+      
+      it "redirects to the login page" do
+        response.should redirect_to("/session/new")
+      end
+    end
+  end
+  
+  describe "POST create" do
+    describe "for a logged in user" do
+      before(:each) do
+        UserSession.create(users(:zack))
+      end
+      
+      it "redirects to the user's account" do
+        post 'create', :user => {'some' => 'params'}
+        response.should redirect_to(account_path)
+      end
+      
+      it "sets the flash" do
+        post 'create', :user => {'some' => 'params'}
+        flash[:notice].should == "You must be logged out to access this page"
+      end
+    end
+    
+    describe "without a logged in user" do
+      describe "with valid params" do
+        it "creates the user" do
+          User.should_receive(:new).with({'some' => 'params'}).and_return(mock_user(:save => true))
+          post 'create', :user => {'some' => "params"}
+          assigns(:user).should equal(mock_user)
+        end
+      end
+      
+      describe "with invalid params" do
+        it "exposes a newly created but unsaved user as @user" do
+          User.should_receive(:new).with({'some' => 'params'}).and_return(mock_user(:save => false))
+          post 'create', :user => {'some' => 'params'}
+          assigns(:user).should equal(mock_user)
+        end
+        
+        it "re-renders the 'new' template" do
+          User.should_receive(:new).and_return(mock_user(:save => false))
+          post 'create', :user => {}
+          response.should render_template("users/new.html.erb")
+        end
+      end
+    end
+  end
+  
+  describe "PUT update" do
+    describe "for a logged in user" do
+      before(:each) do
+        UserSession.create(users(:ben))
+      end
+      
+      describe "with valid attributes" do
+        it "updates the logged in user" do
+          put :update, :user => {}
+          assigns(:user).should == users(:ben)
+        end
 
-  it "should reject show requests if there is no session" do
-    get 'show'
-    response.should redirect_to("/session/new")
-  end
-  
-  it "should reject edit if there is no session" do
-    get 'edit'
-    response.should redirect_to("/session/new")
-  end
-  
-  it "should reject update if there is no session" do
-    post 'update'
-    response.should redirect_to("/session/new")
-  end
-  
-  it "should respond to new with new.html.erb if there's no user logged in" do
-    get 'new'
-    response.should render_template('users/new.html.erb')
-  end
-  
-  it "should respond to show with show.html.erb for logged in user" do
-    UserSession.create(users(:ben))
-    get 'show'
-    assert assigns[:user] == users(:ben) # make sure we're showing the right user!
-    response.should render_template('users/show.html.erb')
-  end
-  
-  it "should respond to edit with edit.html.erb for logged in user" do
-    UserSession.create(users(:zack))
-    get 'edit'
-    assert assigns[:user] == users(:zack) # make sure we're editing the right user!
-    response.should render_template('users/edit.html.erb')
-  end
-  
-  it "should respond to update with logged in user" do
-    UserSession.create(users(:ben))
-    post 'update'
-    response.should redirect_to("/account")
+        it "redirects to the user's account" do
+          put :update, :user => {}
+          response.should redirect_to(account_path)
+        end
+      end
+      
+      describe "with invalid attributes" do
+        it "re-renders the 'edit' template" do
+          put :update, :user => {:password => "pass", :password_confirmation => "wrong!"}
+          response.should render_template("users/edit.html.erb")
+        end
+      end
+    end
   end
 end
