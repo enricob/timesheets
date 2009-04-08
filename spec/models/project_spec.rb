4981306009bd103f63b3c2b27c0277efbca9bbb0
Enrico Bianco <enricob@gmail.com>
2009-04-08 19:39:00 -0400

    added spec to check if Projects accept nested attributes for ActivityTypes and updated Projects model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Project do
+  dataset :projects
+  
   before(:each) do
     @valid_attributes = {
       :name => "value for name",
@@ -14,4 +16,11 @@ describe Project do
   
   it { should validate_presence_of(:name) }
   it { should have_many(:activity_types) }
+  
+  it "should accept nested attributes values for activity types" do
+    test_proj = projects(:project_x)
+    new_attributes = {:activity_types_attributes => {"3" => {:name => "QA", :description => "QA Tasks"}}}
+    assert test_proj.update_attributes!(new_attributes)
+    # The above should throw an exception if Projects do not accept nested attribute values
+  end
 end
