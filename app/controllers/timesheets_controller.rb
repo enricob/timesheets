6eee1c3b316663029cad3d13f2c06ee4a443a490
Enrico Bianco <enricob@gmail.com>
2009-04-10 19:55:55 -0400

    Timesheets controller now builds out a blank entry on a new sheet in editing

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -8,9 +8,10 @@ class TimesheetsController < ApplicationController
   def edit
     @user = @current_user
     @date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-    @timesheet = Timesheet.find_by_start_date(@date)
+    @timesheet = Timesheet.scoped_by_user_id(@user.id).find_by_start_date(@date)
     unless @timesheet
       @timesheet = Timesheet.create({:user => @user, :start_date => @date})
+      @timesheet.time_entries.build
     end
   end
 end
