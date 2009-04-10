1231e045b35cd2d1439f5aadd77659b16d699829
Enrico Bianco <enricob@gmail.com>
2009-04-10 01:14:46 -0400

    Projects show view now lists activity types

--- spec/views/projects/show.html.erb_spec.rb
+++ spec/views/projects/show.html.erb_spec.rb
@@ -7,12 +7,19 @@ describe "/projects/show.html.erb" do
       :name => "value for name",
       :description => "value for description"
     )
+    @project.activity_types.create(:name => "Some Activity", :description => "Some Description")
   end
 
   it "renders attributes in <p>" do
     render
     response.should have_text(/value\ for\ name/)
     response.should have_text(/value\ for\ description/)
+  end
+  
+  it "renders activity types in <p>" do
+    render
+    response.should have_text(/Some\ Activity/)
+    response.should have_text(/Some\ Description/)
   end
 end
 
