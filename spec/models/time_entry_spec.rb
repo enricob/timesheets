65ba642937381fb553306e1f2662fc6b7c2fce25
Enrico Bianco <enricob@gmail.com>
2009-04-10 16:18:32 -0400

    cleaned up spec for validating hours is an integer in TimeEntries specs

--- spec/models/time_entry_spec.rb
+++ spec/models/time_entry_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe TimeEntry do
-  dataset :users, :projects, :activity_types
+  dataset :users, :projects, :activity_types, :time_entries
   
   before(:each) do
     @valid_attributes = {
@@ -21,8 +21,8 @@ describe TimeEntry do
   it { should validate_presence_of(:activity_type).with_message("must be selected") }
   it { should validate_numericality_of(:hours) }
   it "should verify that hours is an integer" do
-    @valid_attributes.merge!(:hours => 0.1)
-    time_entry = TimeEntry.create(@valid_attributes)
+    time_entry = time_entries(:ben1)
+    time_entry.update_attributes({:hours => 0.1})
     time_entry.should have(1).error_on(:hours)
   end
 end
