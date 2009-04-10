f2a9970f8aa81536e3508f15ef5a65fcc5d48a0c
Enrico Bianco <enricob@gmail.com>
2009-04-10 19:59:20 -0400

    Changed Timesheet.create to Timesheet.new so that we don't end up duplicating empty sheets for a particular week that hasn't been entered and saved yet

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -10,7 +10,7 @@ class TimesheetsController < ApplicationController
     @date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
     @timesheet = Timesheet.scoped_by_user_id(@user.id).find_by_start_date(@date)
     unless @timesheet
-      @timesheet = Timesheet.create({:user => @user, :start_date => @date})
+      @timesheet = Timesheet.new({:user => @user, :start_date => @date})
       @timesheet.time_entries.build
     end
   end
