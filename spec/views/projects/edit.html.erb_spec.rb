942905142f8b29b6a4a8763d0bb7af9459e5d226
Enrico Bianco <enricob@gmail.com>
2009-04-10 01:11:34 -0400

    updated specs for Project new and edit views to reflect nesting of ActivityTypes

--- spec/views/projects/edit.html.erb_spec.rb
+++ spec/views/projects/edit.html.erb_spec.rb
@@ -9,14 +9,28 @@ describe "/projects/edit.html.erb" do
       :name => "value for name",
       :description => "value for description"
     )
+    @project.activity_types.create(:name => "Some Activity", :description => "Some Stuff")
   end
 
-  it "renders the edit project form" do
+  it "renders the project form fields" do
     render
     
     response.should have_tag("form[action=#{project_path(@project)}][method=post]") do
       with_tag('input#project_name[name=?]', "project[name]")
       with_tag('textarea#project_description[name=?]', "project[description]")
+    end
+  end
+  
+  it "renders the activity type fields" do
+    render
+    
+    response.should have_tag("div#activity_types") do
+      with_tag('input#project_activity_types_attributes_0_id[name=?][type=?]', 
+        "project[activity_types_attributes][0][id]", "hidden")
+      with_tag('input#project_activity_types_attributes_0_name[name=?]',
+        "project[activity_types_attributes][0][name]")
+      with_tag('input#project_activity_types_attributes_0_description[name=?]',
+        "project[activity_types_attributes][0][description]")
     end
   end
 end
--- spec/views/projects/new.html.erb_spec.rb
+++ spec/views/projects/new.html.erb_spec.rb
@@ -4,11 +4,12 @@ describe "/projects/new.html.erb" do
   include ProjectsHelper
   
   before(:each) do
-    assigns[:project] = stub_model(Project,
+    assigns[:project] = @project = stub_model(Project,
       :new_record? => true,
       :name => "value for name",
       :description => "value for description"
     )
+    @project.activity_types.build # this would happen in controller
   end
 
   it "renders new project form" do
@@ -17,6 +18,17 @@ describe "/projects/new.html.erb" do
     response.should have_tag("form[action=?][method=post]", projects_path) do
       with_tag("input#project_name[name=?]", "project[name]")
       with_tag("textarea#project_description[name=?]", "project[description]")
+    end
+  end
+  
+  it "renders the activity type fields for a new record" do
+    render
+    
+    response.should have_tag("div#activity_types") do
+      with_tag('input#project_activity_types_attributes_0_name[name=?]',
+        "project[activity_types_attributes][0][name]")
+      with_tag('input#project_activity_types_attributes_0_description[name=?]',
+        "project[activity_types_attributes][0][description]")
     end
   end
 end
