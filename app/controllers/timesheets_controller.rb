6830799c6f4afe4d28a4be1175654e84ed4fe092
Enrico Bianco <enricob@gmail.com>
2009-04-10 17:36:23 -0400

    Timesheets Helper provides methods to help find the beginning of the week and build a path from it

--- app/controllers/timesheets_controller.rb
+++
@@ -1,7 +0,0 @@
-class TimesheetsController < ApplicationController
-  before_filter :require_user
-  
-  def index
-    @user = @current_user
-  end
-end
--- app/helpers/timesheets_helper.rb
+++ app/helpers/timesheets_helper.rb
@@ -1,2 +1,11 @@
 module TimesheetsHelper
+  def date_path(date)
+    "#{date.year}/#{date.month}/#{date.day}"
+  end
+  
+  def this_week
+    today = Date.today
+    offset = 1 - today.wday
+    today + offset.days
+  end
 end
