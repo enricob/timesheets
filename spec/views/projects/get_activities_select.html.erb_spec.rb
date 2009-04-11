057fa2116806733982b6e337076d9ed4c96e8638
Enrico Bianco <enricob@gmail.com>
2009-04-11 14:27:57 -0400

    specs added for /projects/get_activities_select.html.erb

---
+++ spec/views/projects/get_activities_select.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe "/projects/get_activities_select.html.erb" do
+  include ProjectsHelper
+  
+  before(:each) do
+    assigns[:project] = @project = stub_model(Project,
+      :id => 37, :name => "TestProject", :description => "Test")
+    @project.activity_types.create(:name => "Activity1")
+    @project.activity_types.create(:name => "Activity2")
+    assigns[:form_id] = @form_id = "my_form_id"
+    assigns[:form_name] = @form_name = "my_form_name"
+  end
+  
+  it "should render a select tag with form_id and form_name" do
+    render
+    response.should have_tag("select[id=?][name=?]", @form_id, @form_name)
+  end
+  
+  it "should render the activity types associated with the project as option tags" do
+    render
+    response.should have_tag("option[value=?]", @project.activity_types.first.id,
+      :text => "Activity1")
+    response.should have_tag("option[value=?]", @project.activity_types.second.id,
+      :text => "Activity2")
+  end
+end
