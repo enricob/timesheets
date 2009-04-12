c412ec71e4884d400b4517e5af9054b914df59dc
Enrico Bianco <enricob@gmail.com>
2009-04-12 19:19:29 -0400

    added specs for TimesheetsHelper

--- spec/helpers/timesheets_helper_spec.rb
+++ spec/helpers/timesheets_helper_spec.rb
@@ -1,11 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe TimesheetsHelper do
+  include TimesheetsHelper
   
-  #Delete this example and add some real ones or delete this file
-  it "should be included in the object returned by #helper" do
-    included_modules = (class << helper; self; end).send :included_modules
-    included_modules.should include(TimesheetsHelper)
+  before(:each) do
+    @date = Date.today
   end
-
+  
+  describe "#date_path" do
+    it "should return a relative path built from the given date" do
+      date_path(@date).should == "#{@date.year}/#{@date.month}/#{@date.day}"
+    end
+  end
+  
+  describe "#week_array" do
+    it "should build an array containing all days within the week of the given date" do
+      test_array = week_array(@date)
+      for d in (@date.beginning_of_week...@date.beginning_of_week + 7.days) do
+        assert test_array.include? d
+      end
+    end
+  end
 end
